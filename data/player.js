a90c9792f3538c7805a9e22aaa81748aa18a87ff
Maksim Pavlenko <maksimp86@mail.ru>
2016-05-10 17:31:32 +0300

    improved application

--- data/player.js
+++ data/player.js
@@ -12,17 +12,29 @@ var listRadio = [
     ["Русское радио", "http://online-rusradio.tavrmedia.ua:8000/RusRadio"],
     ["Авторадио", "http://cast.radiogroup.com.ua:8000/avtoradio"],
     ["ЛюксFM", "http://icecastlv.luxnet.ua/lux"],
+    ["Стильное радио", "http://radio.urg.com.ua/radio-stilnoe"],
     ["RadioMIX Днепр", "http://media.fregat.com:8000/RadioMIX"],
+    ["Радио Вести", "http://212.26.132.60:8000/vesti_mp3"],
+    ["Radio Velykyh Dorig", "http://195.200.251.29:8000/live"],
+    ["Громадське", "http://193.105.70.76:8000/stream?97"],
+    ["UR-3", "http://nrcu.gov.ua:8000/ur3-mp3"],
     ["UAFM", "http://live.uafm.org:9001/uafm"],
     ["Детские песни", "http://music.myradio.com.ua:8000/kids-songs128.mp3"],
     ["SUN FM", "http://radio.sunfm.com.ua:8000/live"],
     ["Radio Metal", "http://s3.radioheart.ru:8015/radio-metal"],
     ["Яскраве Радiо", "http://vobu.com.ua:8000/yaskrave-128"],
+    ["Radio Aristocrats", "http://144.76.79.38:8000/live2"],
     ["Lounge FM", "http://cast.loungefm.com.ua/loungefm?1383955394702.mp3"],
     ["Rock FM", "http://212.90.40.9:8000/rockfm"],
+    ["New day", "http://online.1019.kr.ua:8000/radio"],
     ["Радио правильного репа", "http://rpr-stream.pp.ua:8005/rpr"],
     ["Radiomatch", "http://168.144.82.139:8000/;stream/1"],
     ["Родинне радiо", "http://radio.ukraudio.com:8000/rodynne-radio"],
+    ["Feel (Одесса)", "http://62.80.190.246:8000/Feel"],
+    ["Chernigiv Wave", "http://46.149.84.42:8000/live.ogg"],
+    ["UH Radio (Тернополь)", "http://online.uhradio.com.ua:8001/efir"],
+    ["Рiвне FM", "http://rivne.fm:8000/live"],
+    ["Blacksea wave", "http://85.238.105.86:7052/72,92_FM.mp3"],
     ["Азовская столица", "http://s02.radio-tochka.com:5270/radio"],
     ["Big radio", "http://s02.radio-tochka.com:6840/radio"],
     ["TrancEuphoria", "http://31.184.236.6:8000/tcr128"],
@@ -110,6 +122,7 @@ function clickReverse() {
 function clickPlaylist(event) {
     markItem();
     event.target.style.backgroundColor = "gray";
+    play();
 }
 
 function volume() {
